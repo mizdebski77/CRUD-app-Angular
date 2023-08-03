9be26fd50dac3a0c4370c49aef8ad72517162962
Marcin Izdebski <mizdebski123@gmail.com>
2023-08-03 22:06:41 +0200

    Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.html',
+  styleUrls: ['./app.scss']
 })
 export class AppComponent {
   title = 'crud-app';
