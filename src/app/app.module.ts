313193f1e9ad2ad738809688c41a2d66085796e3
Marcin Izdebski <mizdebski123@gmail.com>
2023-08-03 22:15:45 +0200

    Home component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app';
+import { HomeComponent } from './features/home/home';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
